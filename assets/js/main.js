ee068fccd70f973bfc6e0f375faaa12e20ddca0b
Doomsvylle <123514777+Doomsvylle@users.noreply.github.com>
2026-06-07 19:09:12 +0000

    commit-mostrartabproductos107.06.26

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Mostrar productos por defecto
-    mostrarTab('productos');
+    //mostrarTab('productos');
 
     // ---- MENÚ HAMBURGUESA ----
     const hamburger = document.querySelector('.hamburger');
